6a81fdb3961c8a665883259eb118851b12342729
rjzhb <rjzhb222@163.com>
2023-01-05 13:15:20 +0800

    update k-slack

--- src/kslack/k_slack.cpp
+++ src/kslack/k_slack.cpp
@@ -9,9 +9,23 @@ void KSlack::disorder_handling() {
 
     while (!stream_list_.empty()) {
         //先让缓冲区所有满足条件的tuple出队进入输出区
-        while(!buffer_.empty()){
+        while (!buffer_.empty()) {
             Tuple tuple = *buffer_.begin();
-            if(tuple.ts + buffer_size_ <=
+
+            //TODO: 用BufferManager动态更新buffer_size_(论文中的K), 把事情交给BufferManager
+
+            //对应论文的公式：ei. ts + Ki <= T
+            if (tuple.ts + buffer_size_ > current_time_) {
+                break;
+            }
+            //满足上述公式，加入输出区
+            output_list_.push(tuple);
+            buffer_.erase(buffer_.begin());
         }
+
+        Tuple tuple = stream_list_.front();
+        stream_list_.pop();
+        //加入tuple进入buffer
+        buffer_.insert(tuple);
     }
 }
